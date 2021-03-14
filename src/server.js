e4e3236b45e9c7c412ec84075de5bacd7ff1182a
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2021-03-14 18:46:04 -0400

    merge from main

--- src/server.js
+++ src/server.js
@@ -18,25 +18,25 @@ app.use((error,req,res,next) => {
     res.status(500).json(response)
 })
 
-const db = knex({
-    client: 'pg',
-    connection: {
-        host: 'ec2-3-232-163-23.compute-1.amazonaws.com',
-        user: 'tpicoklvgbcetc',
-        password: '85fa17e2852d2998d6fbb50a27110f2986a0f027acc92e93ef354bcb3b273878',
-        database: 'd2e5j53020b0rd',
-    }
-})
-
 // const db = knex({
-//     client:  'pg',
+//     client: 'pg',
 //     connection: {
-//         host: 'localhost',
-//         user: 'postgres',
-//         password: '',
-//         database: 'drink-social'
+//         host: 'ec2-3-232-163-23.compute-1.amazonaws.com',
+//         user: 'tpicoklvgbcetc',
+//         password: '85fa17e2852d2998d6fbb50a27110f2986a0f027acc92e93ef354bcb3b273878',
+//         database: 'd2e5j53020b0rd',
 //     }
 // })
+
+const db = knex({
+    client:  'pg',
+    connection: {
+        host: 'localhost',
+        user: 'postgres',
+        password: '',
+        database: 'drink-social'
+    }
+})
 
 app.set('db',db)
 
