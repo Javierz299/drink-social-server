a8cdb34083e236719a61d1e987b7c7b714e3e4a2
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2021-03-17 00:24:31 -0400

    changed routes back to local

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 
-    "postgres://tpicoklvgbcetc:85fa17e2852d2998d6fbb50a27110f2986a0f027acc92e93ef354bcb3b273878@ec2-3-232-163-23.compute-1.amazonaws.com:5432/d2e5j53020b0rd",
+    "postgresql://postgres:@localhost/drink-social",
 }
